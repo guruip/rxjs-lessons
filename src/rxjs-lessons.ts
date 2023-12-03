689c214dbe0742dd0ac6b09bbc9bd374f5031e43
Mac <mac@MacBook-Pro-Mac.local>
2023-12-03 17:35:24 +0300

    add new chenges

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -1,22 +1,53 @@
 import { Observable, fromEvent } from "rxjs";
-import { map, debounceTime, distinctUntilChanged } from "rxjs/operators";
+import { map, debounceTime, distinctUntilChanged, takeUntil } from "rxjs/operators";
 
 
-// const search$ = new Observable(observer => {
+// const search$ = new Observable<Event>(observer => {
 //     const search = document.getElementById('search');
+//     const stop = document.getElementById('stop');
 
-//     if (!search) {
+//     if (!search || !stop) {
 //         observer.error("error");
 //         return;
 //     }
 
-//     search.addEventListener('input', event => {
+//     const onSearch = event => {
+//         checkSubscription();
 //         observer.next(event);
-//     })
+//     }
+
+//     const onStop = event => {
+//         checkSubscription();
+//         observer.complete();
+//         clear();
+//     }
+
+//     search.addEventListener('input', onSearch);
+//     stop.addEventListener('click', onStop);
+
+//     const checkSubscription = () => {
+//         if (observer.closed) {
+//             clear();
+//         }
+//     }
+
+
+//     const clear = () => {
+//         search.removeEventListener('input', onSearch);
+//         stop.removeEventListener('click', onStop);
+
+//     }
 // });
+
+
 const search$: Observable<Event> = fromEvent<Event>(
     document.getElementById('search'),
     'input'
+)
+
+const stop$: Observable<Event> = fromEvent<Event>(
+    document.getElementById('stop'),
+    'click'
 )
 
 search$.pipe(
@@ -25,13 +56,12 @@ search$.pipe(
     }),
     debounceTime(500),
     map(value => value.length > 3 ? value : ''),
-    distinctUntilChanged()
+    distinctUntilChanged(),
+    takeUntil(stop$)
 ).subscribe(value => {
     console.log(value);
 })
 
 search$.subscribe( value => {
-    console.log(value);
+    // console.log(value);
 }, err => console.log(err));
-
-console.log("end");
